7e3b7bfee919e5313875fc3c24cc081710da4c06
kongo2002 <kongo2002@googlemail.com>
2009-06-06 13:46:52 +0200

    added uniquer to history loading

--- .uzbl/scripts/load_url_from_history.sh
+++ .uzbl/scripts/load_url_from_history.sh
@@ -4,7 +4,7 @@ history_file=$HOME/.uzbl/history
 COLORS=" -nb #303030 -nf khaki -sb #CCFFAA -sf #303030"
 OPTIONS=" -i -l 10"
  
-goto=`< $history_file dmenu $OPTIONS $COLORS | cut -d " " -f 3 | uniq | awk '{print $NF}'`
+goto=`cut -d " " -f 3 $history_file | uniquer | dmenu $OPTIONS $COLORS`
  
 #[ -n "$goto" ] && uzblctrl -s $5 -c "uri $goto"
 [ -n "$goto" ] && echo "act uri $goto" > $4
