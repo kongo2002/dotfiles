35fbd36ba3466a0c3490e39c5db1ae0ef5808878
kongo2002 <kongo2002@googlemail.com>
2010-12-23 23:21:30 +0100

    modify git prompt

--- .zsh/git.zsh
+++ .zsh/git.zsh
@@ -1,11 +1,11 @@
 # git status colors
 
-ZSH_THEME_GIT_PROMPT_CLEAN="%{${fg_bold[green]}%}"
-ZSH_THEME_GIT_PROMPT_UNTRACKED="%{${fg_bold[green]}%}"
-ZSH_THEME_GIT_PROMPT_ADDED="%{${fg_bold[yellow]}%}"
-ZSH_THEME_GIT_PROMPT_MODIFIED="%{${fg_bold[yellow]}%}"
-ZSH_THEME_GIT_PROMPT_RENAMED="%{${fg_bold[yellow]}%}"
-ZSH_THEME_GIT_PROMPT_UNMERGED="%{${fg_bold[red]}%}"
+ZSH_THEME_GIT_PROMPT_ADDED="%{${fg_bold[green]}%}+"
+ZSH_THEME_GIT_PROMPT_UNTRACKED="%{${fg_bold[yellow]}%}?"
+ZSH_THEME_GIT_PROMPT_MODIFIED="%{${fg_bold[yellow]}%}*"
+ZSH_THEME_GIT_PROMPT_RENAMED="%{${fg_bold[yellow]}%}→"
+ZSH_THEME_GIT_PROMPT_UNMERGED="%{${fg_bold[red]}%}!"
+ZSH_THEME_GIT_PROMPT_DELETED="%{${fg_bold[red]}%}-"
 
 # get the current branch
 
@@ -20,23 +20,31 @@ git_prompt () {
     INDEX=$(git status --porcelain 2> /dev/null)
     GIT=""
 
-    if $(echo "$INDEX" | grep '^UU ' &> /dev/null); then
-        GIT="$ZSH_THEME_GIT_PROMPT_UNMERGED"
-    elif $(echo "$INDEX" | grep '^A ' &> /dev/null); then
-        GIT="$ZSH_THEME_GIT_PROMPT_ADDED"
-    elif $(echo "$INDEX" | grep '^M ' &> /dev/null); then
-        GIT="$ZSH_THEME_GIT_PROMPT_ADDED"
-    elif $(echo "$INDEX" | grep '^ M ' &> /dev/null); then
-        GIT="$ZSH_THEME_GIT_PROMPT_MODIFIED"
-    elif $(echo "$INDEX" | grep '^R ' &> /dev/null); then
-        GIT="$ZSH_THEME_GIT_PROMPT_RENAMED"
-    elif $(echo "$INDEX" | grep '^ D ' &> /dev/null); then
-        GIT="$ZSH_THEME_GIT_PROMPT_DELETED"
-    elif $(echo "$INDEX" | grep '^?? ' &> /dev/null); then
-        GIT="$ZSH_THEME_GIT_PROMPT_UNTRACKED"
-    else
-        GIT="$ZSH_THEME_GIT_PROMPT_CLEAN"
+    if $(echo "$INDEX" | grep '^?? ' &> /dev/null); then
+        GIT="$ZSH_THEME_GIT_PROMPT_UNTRACKED$GIT"
     fi
 
-    echo "$GIT($(current_branch))%{${reset_color}%}"
+    if $(echo "$INDEX" | grep '^A ' &> /dev/null); then
+        GIT="$ZSH_THEME_GIT_PROMPT_ADDED$GIT"
+    elif $(echo "$INDEX" | grep '^M' &> /dev/null); then
+        GIT="$ZSH_THEME_GIT_PROMPT_ADDED$GIT"
+    fi
+
+    if $(echo "$INDEX" | grep '^R ' &> /dev/null); then
+        GIT="$ZSH_THEME_GIT_PROMPT_RENAMED$GIT"
+    fi
+
+    if $(echo "$INDEX" | grep '^ D ' &> /dev/null); then
+        GIT="$ZSH_THEME_GIT_PROMPT_DELETED$GIT"
+    fi
+
+    if $(echo "$INDEX" | grep '^[A-Z ]M ' &> /dev/null); then
+        GIT="$ZSH_THEME_GIT_PROMPT_MODIFIED$GIT"
+    fi
+
+    if $(echo "$INDEX" | grep '^UU ' &> /dev/null); then
+        GIT="$ZSH_THEME_GIT_PROMPT_UNMERGED$GIT"
+    fi
+
+    echo "$GIT %{${reset_color}%}($(current_branch))"
 }
