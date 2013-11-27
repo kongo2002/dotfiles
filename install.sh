25128def22a8a5b2e4b032c4c7b17753ca8b3c57
kongo2002 <kongo2002@googlemail.com>
2013-11-28 00:22:49 +0100

    install.sh: improve error message

--- install.sh
+++ install.sh
@@ -10,9 +10,13 @@ pushd "$HOME"
 for f in $FILES; do
     if [ ! -e "$HOME/.$f" ]; then
         echo "Creating symlink from .$f to $REPO/$f"
-	ln -s "$REPO/$f" ".$f"
+        ln -s "$REPO/$f" ".$f"
     else
-        echo ".$f already exists"
+        if [ -h "$HOME/.$f" ]; then
+            echo "'.$f' already exists"
+        else
+            echo "'.$f' is a regular file/folder. Remove it first!"
+        fi
     fi
 done
 
