56cd80ae40e6daceb06854492443319392228e43
kongo2002 <kongo2002@googlemail.com>
2014-05-16 00:58:22 +0200

    add grc config

---
+++ zsh/grc.zsh
@@ -0,0 +1,21 @@
+GRC=`which grc`
+
+if [ "$TERM" != dumb ] && [ -n GRC ]; then
+    alias colorize="$GRC -es --colour=auto"
+
+    alias as='colorize as'
+    alias configure='colorize ./configure'
+    alias diff='colorize diff'
+    alias g++='colorize g++'
+    alias gas='colorize gas'
+    alias gcc='colorize gcc'
+    alias ifconfig='colorize ifconfig'
+    alias ld='colorize ld'
+    alias make='colorize make'
+    alias mount='colorize mount'
+    alias netstat='colorize netstat'
+    alias ping='colorize ping -c 5'
+    alias ps='colorize ps'
+    alias traceroute='colorize traceroute'
+    alias xbuild='colorize xbuild'
+fi
