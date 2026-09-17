ab7514c03ed1823cff743cbdb9ff40e2748b4f6f
kongo2002 <kongo2002@googlemail.com>
2026-09-18 01:54:10 +0200

    zsh: use eza if available

--- zsh/darwin.zsh
+++ zsh/darwin.zsh
@@ -4,6 +4,12 @@ if [[ "$(uname -s)" != "Darwin" ]]; then
     return 0
 fi
 
-alias ls='ls -hG'
-alias ll='ls -lhG'
-alias l='ls -lhaG'
+if [[ -x $(which eza) ]]; then
+    alias ls='eza'
+    alias ll='eza -l --git --no-permissions --octal-permissions'
+    alias l='eza -la --git --no-permissions --octal-permissions'
+else
+    alias ls='ls -hG'
+    alias ll='ls -lhG'
+    alias l='ls -lhaG'
+fi
--- zsh/linux.zsh
+++ zsh/linux.zsh
@@ -4,6 +4,14 @@ if [[ "$(uname -s)" != "Linux" ]]; then
     return 0
 fi
 
-alias ls='ls -h --color=auto'
-alias ll='ls -lh --color=auto'
-alias l='ls -lha --color=auto'
+if [[ -x $(which eza) ]]; then
+    alias ls='eza'
+    alias ll='eza -l --git --no-permissions --octal-permissions'
+    alias l='eza -la --git --no-permissions --octal-permissions'
+else
+    alias ls='ls -h --color=auto'
+    alias ll='ls -lh --color=auto'
+    alias l='ls -lha --color=auto'
+fi
+
+export PLAYWRIGHT_BROWSERS_PATH=~/.ms-playwright/
